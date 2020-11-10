dcf6d5b082d296970f77efbafa47c84b0ad1c0ac
Quentin Neyraud <quentin.neyraud@gmail.com>
2020-11-10 13:34:09 +0100

    Initial commit

---
+++ components/inputs/Switcher.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="Switcher-component">
+    <div class="Switcher-options">
+      <div
+        v-for="(option, optionIndex) in options"
+        :key="optionIndex"
+        class="Switcher-option"
+        @click="currentOption = option.value"
+      >
+        <span>{{ option.text }}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    options: {
+      type: Array,
+      required: true
+    }
+  },
+  data () {
+    return {
+      currentOption: this.options?.[0]?.value
+    }
+  },
+  watch: {
+    currentOption (currentOption) {
+      this.$emit('change', {
+        option: currentOption
+      })
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.Switcher-component
+  position relative
+  margin-top 25px
+
+.Switcher-options
+  border-radius 10px
+  height 50px
+  display inline-flex
+  overflow hidden
+
+.Switcher-option
+  background-color grey
+  min-width 100px
+  flex-parent-center()
+
+  span
+    color white
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="Home-page">
+    <h1>Nuxt configuration helper</h1>
+
+    <Switcher
+      :options="[{
+        text: 'ok',
+        value: 'ok'
+      }]"
+    />
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
